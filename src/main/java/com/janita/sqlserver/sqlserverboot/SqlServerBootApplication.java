606fb6c02d4621f5fe7bddaa4fce39d8d80506c2
janita <zcj880902>
2017-06-29 17:34:28 +0800

    Initial commit

---
+++ src/main/java/com/janita/sqlserver/sqlserverboot/SqlServerBootApplication.java
@@ -0,0 +1,12 @@
+package com.janita.sqlserver.sqlserverboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlServerBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlServerBootApplication.class, args);
+	}
+}
