7cc4ddb3ce5590c9db0d6da56ebc54a5995796cc
janita <zcj880902>
2017-06-29 19:37:25 +0800

    sqlserver

---
+++ sqlserver-jpa/src/main/java/com/janita/jpa/JpaApplication.java
@@ -0,0 +1,15 @@
+package com.janita.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Janita on 2017/6/29 0029-下午 7:35
+ * 该类是：
+ */
+@SpringBootApplication
+public class JpaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JpaApplication.class, args);
+    }
+}
